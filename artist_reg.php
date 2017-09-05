ffc3f586b7d1c41b6fd7bbf1268de3923de94102
crayon7 <31586335+crayon7@users.noreply.github.com>
2017-09-06 01:33:01 +0630

    artist page edit

--- artist_reg.php
+++ artist_reg.php
@@ -1,40 +1,57 @@
 <?php
-	require_once('dbconnect.php');
+require_once('dbconnect.php');
 
-	if(isset($_POST['btnRegister'])){
-		
-		$artistname = $_POST['txtArtistname'];
-		$gender      = $_POST['genre'];
-		$phone      = $_POST['txtPh'];
-		$address    = $_POST['txtAddress'];
-		$description = $_POST['txtDescription'];
+if(isset($_POST['btnRegister'])){
 
-		if(empty($artistname)){
-			$msg = "Please enter artistname";
-		}
-		else if(empty($gender)){
-			$msg = "Please choose gender";
-		}else if(empty($phone)){
-			$msg = "Please enter phone number";
-		}else if(empty($address)){
-			$msg = "Please enter address";
-		}else if(empty($description)){
-			$msg = "please write description";
-		}else{
-			connect();
-			$query = mysql_query("select * from artist where artist_name = '$artistname' and phone = '$phone'") or die("Cannot Select");
-			if(mysql_num_rows($query)>0){
-				$msg = "This artist is already exit";
-			}else{
-				$query1 = mysql_query("insert into artist(artist_name,genre,phone,address,description) VALUES ('$artistname',
-					'$gender','$phone','$address','$description')") or die("Cannot Insert".mysql_error());
-				if($query1){
-					$msg = "Save Successfully Record";
-				}
-			}
-		}
+	$artistname = $_POST['txtArtistname'];
 
+	$genre = $_POST['genre'];
+
+	$list = implode(',',$genre);
+// for each value
+
+	for($i = 0;count($genre)<$i;$i++){
+
+    // put insert query and value of selected price
+		$list = $genre[$i];
 	}
+
+}
+	$phn = $_POST['txtPh'];
+$phone      = "(".substr($phn,0,2).") ".substr($phn,2,3)."-".substr($phn,5,3)."-".substr($phn,8);	
+$address    = $_POST['txtAddress'];
+$description = mysql_real_escape_string($_POST['txtDescription']);
+
+if(empty($artistname)){
+	$msg = "Please enter artistname";
+}
+else if(empty($list)){
+	$msg = "Please choose gender";
+}else if (count($genre)>3) {
+	$msg = "Please Select only 3";
+}else if(empty($phone)){
+	$msg = "Please enter phone number";
+}else if(count($phn)>11){
+	$msg = "Wrong phone number_format";
+}else if(empty($address)){
+	$msg = "Please enter address";
+}else if(empty($description)){
+	$msg = "please write description";
+}else{
+	connect();
+	$query = mysql_query("select * from artist where artist_name = '$artistname' and phone = '$phone'") or die("Cannot Select");
+	if(mysql_num_rows($query)>0){
+		$msg = "This artist is already exit";
+	}else{
+		$query1 = mysql_query("insert into artist(artist_name,genre,phone,address,description) VALUES ('$artistname',
+			'$list','$phone','$address','$description')") or die("Cannot Insert".mysql_error());
+		if($query1){
+			$msg = "Save Successfully Record";
+		}
+	}
+}
+
+
 
 
 ?>
@@ -46,60 +63,60 @@
 </head>
 <body>
 	<?php
-		if(!empty($msg)){
-			echo "<font color = \"red\">".$msg."</font>";
-		}
+	if(!empty($msg)){
+		echo "<font color = \"red\">".$msg."</font>";
+	}
 	?>
 	<form method = "POST" action = "">
 		<table border="1px" border-padding="10px">
-		<tr>
+			<tr>
 				<thead>
-				<td colspan="2">Registeration</td>
-			</thead>
-																</tr>
-		
-		<tbody>
-		<tr>
-				<td>Name</td>
-				<td><input type="text" name="txtArtistname"></td>
-		</tr>
+					<td colspan="2">Registeration</td>
+				</thead>
+			</tr>
 
-		
+			<tbody>
+				<tr>
+					<td>Name</td>
+					<td><input type="text" name="txtArtistname"></td>
+				</tr>
 
-		<tr>
-			<td>Gender</td>
-			<td><input type="checkbox" name="genre"   value = "1">Pop
-				<input type="checkbox" name="genre"  value = "2">Rock
-				<input type="checkbox" name="genre" value = "3">Dance/Electronic</br>
-				<input type="checkbox" name="genre"  value = "4">Soul
-				<input type="checkbox" name="genre"   value = "5">R&B
-				<input type="checkbox" name="genre" value = "6">Hip Hop</br>
-				<input type="checkbox" name="genre" value = "7">Other
-			</td>
-			
-		</tr>
-		<tr>
-				<td>Phone</td>
-				<td><input type="text" name="txtPh"></td>
-		</tr>
 
-		<tr>
-				<td>Address</td>
-				<td><input type="text" name="txtAddress"></br>
-					
-		</tr>
-		<tr>
-				<td>Descriptions</td>
-				<td><textarea name="txtDescription">
-					
-				</textarea></td>
-		</tr>
 
-		<tr>
-			<td colspan="2"><button name = "btnRegister">Register</button><button>Back to home</button></td>
-		</tr>
-	</tbody>
-	</table>
-	</form>
-</body>
-</html>
+				<tr>
+					<td>Gender</td>
+					<td><input type="checkbox" name="genre[]" value = "Pop">Pop
+						<input type="checkbox" name="genre[]" value = "Rock">Rock
+						<input type="checkbox" name="genre[]" value = "Dance/Electronic">Dance/Electronic</br>
+						<input type="checkbox" name="genre[]" value = "Soul">Soul
+						<input type="checkbox" name="genre[]" value = "R&B">R&B
+						<input type="checkbox" name="genre[]" value = "Hip Hop">Hip Hop</br>
+						<input type="checkbox" name="genre[]" value = "Other...">Other
+					</td>
+
+				</tr>
+				<tr>
+					<td>Phone</td>
+					<td><input type="text" name="txtPh"></td>
+				</tr>
+
+				<tr>
+					<td>Address</td>
+					<td><input type="text" name="txtAddress"></br>
+
+					</tr>
+					<tr>
+						<td>Descriptions</td>
+						<td><textarea name="txtDescription">
+
+						</textarea></td>
+					</tr>
+
+					<tr>
+						<td colspan="2"><button name = "btnRegister">Register</button><button>Back to home</button></td>
+					</tr>
+				</tbody>
+			</table>
+		</form>
+	</body>
+	</html>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php 
+	$num = "09968336533";  // given                                                  
+$formatted = "(".substr($num,0,2).") ".substr($num,2,3)."-".substr($num,5,3)."-".substr($num,8);
+echo $formatted;
+ ?>
